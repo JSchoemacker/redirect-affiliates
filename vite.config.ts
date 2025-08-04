912e8f52a7a3e714377f3245a1734f6450629718
JSchoemacker <jschemacker@pasino.ch>
2025-08-04 15:08:10 +0200

    fix for pentest

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,17 @@ export default defineConfig({
   build: {
     sourcemap: false
   },
-  server: {
-   port: 3001,
-   strictPort: true,
-   host: true,
-   allowedHosts: true
-  },
+server: {
+  port: 3001,
+  strictPort: true,
+  host: true,
+  allowedHosts: true,
+  fs: {
+    allow: [
+      './'
+    ]
+  }
+},
   preview: {
    allowedHosts: true
   }
